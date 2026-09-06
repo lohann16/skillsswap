cf7b107fd75fae24ea3ad5f497197ee95776954a
Lohann <delohann2002@gmail.com>
2026-09-06 20:27:31 +0200

    feat: add storage export to Firebase configuration

--- Firebase/config.js
+++ Firebase/config.js
@@ -2,7 +2,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
-
+import { getStorage } from 'firebase/storage';
 // Paste the firebaseConfig object Firebase gave you when you registered
 
 const firebaseConfig = {
@@ -19,4 +19,5 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getDatabase(app);
+export const storage = getStorage(app);
 export default app;
